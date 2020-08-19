da23bef9ca431bcf942cb59f5f1cc0bd32ea01b5
David Jordan <djordan6@huskers.unl.edu>
2020-08-19 14:51:51 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: David Jordan
+ *         djordan6@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("David Jordan\n");
+
+  return 0;
+}
